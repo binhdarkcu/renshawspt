3a4269f230758f2d255e3c37323dde1d1b6c6b5c
Thanh Che <cqthanh@tma.com.vn>
2019-01-22 15:58:39 +0700

    update to load from local server

--- wp-content/themes/renshawspt/page-forgot-password.php
+++ wp-content/themes/renshawspt/page-forgot-password.php
@@ -16,7 +16,6 @@ $content = str_replace(']]>', ']]&gt;', $content);
         <div id="page-476" class="type-page">
 
             <!-- page-title -->
-
             <time datetime="2017-05-28"></time>
             <h1 class="page-title">Forgot Password </h1>						<!-- /page-title -->
 
@@ -24,22 +23,7 @@ $content = str_replace(']]>', ']]&gt;', $content);
 
                 <?php echo $content;?>
 
-<!--                <form method="post">
-                    <div class="mm-forgot-password">
-                        <h3>Enter your email address below</h3>
-                        <p><input type="text" id="email" name="email" class="mm-field"><br>
-                            <input type="submit" name="submit" value="Submit" id="mm-submit-button" class="mm-button " onclick="return forgotpassword_js.checkFields();">
-                        </p></div>
-                </form>-->
-                <div id="themify_builder_content-476" data-postid="476" class="themify_builder_content themify_builder_content-476 themify_builder">
-
-                </div>
-                <!-- /themify_builder_content -->
-
-
-                <!-- comments -->
-                <!-- /comments -->
-
+                <div id="themify_builder_content-476" data-postid="476" class="themify_builder_content themify_builder_content-476 themify_builder"></div>
             </div>
             <!-- /.post-content -->
 
